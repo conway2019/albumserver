e832d513bd4f078c36fbe88ba2d4f5b6fb58d64d
conway <kangwei@xinhua.org>
2020-02-18 07:02:02 +0800

    20200217-2

--- bin/main.js
+++ bin/main.js
@@ -9,10 +9,14 @@ const app = require("../app")
 let server = http.createServer(app)
 
 mongoose.connect(mongo, function (err) {
-    console.log("mongo建立连接")
-    if (!err) {
+    if (err) {
+       console.log("mongo建立连接失败");
+       console.log(err);
+    }
+    else{
+        console.log("mongo建立连接成功");
         server.listen(3001, function (err) {
-            if (!err) console.log("监听3001端口")
+            if (!err) console.log("服务监听3001端口")
         })
     }
 })
